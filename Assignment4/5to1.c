b1cee6ad6fa7da7d48e0291a3c9426e1e9860cb0
Kalidass-05 <140417758+Kalidass-05@users.noreply.github.com>
2025-11-29 11:42:13 +0530

    Create 5to1.c

---
+++ Assignment4/5to1.c
@@ -0,0 +1,13 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+   int i=5;
+   loop:if(i>=1)
+   {
+       printf("%d\n",i);
+       i--;
+       goto loop;
+   }
+    return 0;
+}
