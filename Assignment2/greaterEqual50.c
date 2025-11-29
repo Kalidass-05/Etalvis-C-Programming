6c6ba2dece86769c190a5e99624fde8d45a273ab
Kalidass-05 <140417758+Kalidass-05@users.noreply.github.com>
2025-11-29 11:13:22 +0530

    Create greaterEqual50.c

---
+++ Assignment2/greaterEqual50.c
@@ -0,0 +1,10 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    printf("%d",(n>=50));
+    return 0;
+}
