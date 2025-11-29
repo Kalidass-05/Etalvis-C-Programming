1abca4a88dbe39c75424fded861ea5600dbee54e
Kalidass-05 <140417758+Kalidass-05@users.noreply.github.com>
2025-11-29 15:24:15 +0530

    Create odd10to20.c

---
+++ Assignment5/odd10to20.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int sum=0;
+    for(int i=10;i<=20;i++)
+    {
+        if(i%2==1)
+        {
+            printf("%d\n",i);
+        }
+    }
+    return 0;
+}
