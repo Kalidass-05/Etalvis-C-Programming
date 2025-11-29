7c378e6cbc64b550d816dd24b1afa5b9bb92e76c
Kalidass-05 <140417758+Kalidass-05@users.noreply.github.com>
2025-11-29 11:24:34 +0530

    Create 16.c

---
+++ Assignment2/16.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    int a = n%100;
+    int b = (n/100)%100;
+    printf("%d",a==b);
+    return 0;
+}
