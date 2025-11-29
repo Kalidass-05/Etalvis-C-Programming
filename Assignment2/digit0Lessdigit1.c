15bcc933d18818017781edab3923d6861ab86dda
Kalidass-05 <140417758+Kalidass-05@users.noreply.github.com>
2025-11-29 11:18:32 +0530

    Create digit0Lessdigit1.c

---
+++ Assignment2/digit0Lessdigit1.c
@@ -0,0 +1,10 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    printf("%d",(n%10)<(n/10)%10);
+    return 0;
+}
