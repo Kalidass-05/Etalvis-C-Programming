0dca44611f2b201a59455283abb9971ca26d49fd
Kalidass-05 <140417758+Kalidass-05@users.noreply.github.com>
2025-11-29 17:31:00 +0530

    Create CheckLast2DigitPrime.c

---
+++ Assignment5/CheckLast2DigitPrime.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+    int n;
+    printf("Enter the number:");
+    scanf("%d",&n);
+    int f=1;
+    int d = n%100;
+    
+    if(d<2) printf("Not Prime");
+    for(int i=2;i<sqrt(d);i++)
+    {
+        if(d%i==0)
+        {
+            f=0;
+        }
+    }
+    
+    if(f==1) printf("Prime");
+    else printf("Not Prime");
+    return 0;
+}
