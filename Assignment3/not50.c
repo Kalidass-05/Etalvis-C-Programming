4bebd150ebf848be4b7e468cbd65f6ac680127bc
Kalidass-05 <140417758+Kalidass-05@users.noreply.github.com>
2025-11-29 11:27:56 +0530

    Create not50.c

---
+++ Assignment3/not50.c
@@ -0,0 +1,17 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    if(n!=50)
+    {
+        printf("Success");
+    }
+    else
+    {
+        printf("Failure");
+    }
+    return 0;
+}
