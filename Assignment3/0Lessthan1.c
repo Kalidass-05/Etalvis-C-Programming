ba1f14f0e0a1c2f1a1b1d6992037ff7569faf7d9
Kalidass-05 <140417758+Kalidass-05@users.noreply.github.com>
2025-11-29 11:34:03 +0530

    Create 0Lessthan1.c

---
+++ Assignment3/0Lessthan1.c
@@ -0,0 +1,19 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    int a = n%10;
+    int b = (n/10)%10;
+    if(a<b)
+    {
+        printf("Success");
+    }
+    else
+    {
+        printf("Failure");
+    }
+    return 0;
+}
