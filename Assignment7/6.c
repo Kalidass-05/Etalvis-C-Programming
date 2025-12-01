76c99d4d2d216a240e5b9ae90d4508fc6181d1a6
Kalidass-05 <140417758+Kalidass-05@users.noreply.github.com>
2025-12-01 21:39:35 +0530

    Create 6.c

---
+++ Assignment7/6.c
@@ -0,0 +1,13 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+void disp_2digit_odd(int i){
+    if(i%2==1) printf("%d ",i);
+    }
+int main() {
+    for(int i=11;i<=20;i++)
+    {
+        disp_2digit_odd(i);
+    }
+
+    return 0;
+}
