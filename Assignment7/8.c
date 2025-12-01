224a37936f76b7083dbb2728913a9fed38d65dff
Kalidass-05 <140417758+Kalidass-05@users.noreply.github.com>
2025-12-01 21:47:30 +0530

    Create 8.c

---
+++ Assignment7/8.c
@@ -0,0 +1,26 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+void disp_2digit_odd(int n){
+   int sum=0;
+   int temp = n;
+   if(n%2==0)
+   {
+       while(n!=0)
+       {
+           sum+=n%10;
+           n/=10;
+       }
+   }
+   if(sum==6)
+   {
+       printf("%d ",temp);
+   }
+    }
+int main() {
+    for(int i=11;i<=99;i++)
+    {
+        disp_2digit_odd(i);
+    }
+
+    return 0;
+}
