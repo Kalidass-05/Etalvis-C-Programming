4fe4abe9b18afad117f02c8e04d7a12195216472
Kalidass-05 <140417758+Kalidass-05@users.noreply.github.com>
2025-12-01 21:38:01 +0530

    Create 5.c

---
+++ Assignment7/5.c
@@ -0,0 +1,13 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+void is_odd(int i){
+    if(i%2==1) printf("%d ",i);
+    }
+int main() {
+    for(int i=1;i<10;i++)
+    {
+        is_odd(i);
+    }
+
+    return 0;
+}
