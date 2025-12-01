afaa35011003c0603613720593d82db3f53b9f0d
Kalidass-05 <140417758+Kalidass-05@users.noreply.github.com>
2025-12-01 21:31:38 +0530

    Create 2.c

---
+++ Assignment7/2.c
@@ -0,0 +1,17 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    void dsip_ds(int i)
+    {
+        printf("%d\n",i);
+    }
+    int i=5;
+    while(i>=1)
+    {
+        dsip_ds(i);
+        i--;
+    }
+
+    return 0;
+}
